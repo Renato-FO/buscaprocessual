dc4f70e138a06ccc91168c8d71001d369b580c63
Renato <renatoordonho@gmail.com>
2023-12-04 06:16:00 -0300

    s

--- app/api/route.js
+++ app/api/route.js
@@ -12,20 +12,19 @@ export async function POST(req) {
             args: [...chrome.args, "--hide-scrollbars", "--disable-web-security", '--disable-http2'],
             defaultViewport: chrome.defaultViewport,
             executablePath: await chrome.executablePath,
-            headless: chrome.headless,
+            headless: false,
             ignoreHTTPSErrors: true,
         }).then(async browser => {
 
-            const page = await browser.newPage();
-            await page.setViewport({ width: 1280, height: 720 });
-            await page.setExtraHTTPHeaders({
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-                'upgrade-insecure-requests': '1',
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-                'accept-encoding': 'gzip, deflate, br',
-                'accept-language': 'en-US,en;q=0.9,en;q=0.8',
-            });
-            await page.setUserAgent('5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
+            const [page] = await browser.pages();
+            // await page.setExtraHTTPHeaders({
+            //     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+            //     'upgrade-insecure-requests': '1',
+            //     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+            //     'accept-encoding': 'gzip, deflate, br',
+            //     'accept-language': 'en-US,en;q=0.9,en;q=0.8',
+            // });
+            // await page.setUserAgent('5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
 
 
             await page.goto('https://www.google.com/', { waitUntil: "networkidle0" });
